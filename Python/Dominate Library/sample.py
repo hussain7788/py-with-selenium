94a928eb1737db969cf41b5af78c509c9b8b8a70
Hussain Valli Ganganapalli -X (hgangana - INFOSYS LIMITED at Cisco) <hgangana@cisco.com>
2024-11-19 18:35:12 +0530

    Docker steps for build

--- Python/Dominate Library/sample.py
+++ Python/Dominate Library/sample.py
@@ -16,3 +16,24 @@ with doc:
         h1('My Web Page')
 
 print(doc)
+
+### html2text is a Python library that converts HTML to plain text. 
+# It is useful for extracting text content from HTML documents, \
+from dominate.tags import div,span,input_, textarea
+import html2text    
+
+def test_span():
+    return span('Email', input_(type="radio", disabled="true"))
+
+
+def html_to_text(html):
+    return html2text.html2text(html)
+
+
+htm = test_span()
+print(html_to_text(str(htm)))
+
+text_area = div(
+            textarea("I am Hussain", style="height:100px; width:500px;", disabled="true"))
+print(text_area)
+print(html_to_text(str(text_area)))
